227f2f7b7f2a08fff0ae7b575195e8591988eb12
Jared Turkewitz <turkewitz@physics.umn.edu>
2016-09-30 15:12:22 -0500

    added in metadata files

---
+++ submit_jobs2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+# This is a comment
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleMuon2012A.txt --batch=30 --jobname=jpsiTest658_Z_80_100_DoubleMuon_2012A
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleMuon2012B.txt --batch=30 --jobname=jpsiTest658_Z_80_100_DoubleMuon_2012B
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleMuon2012C.txt --batch=30 --jobname=jpsiTest658_Z_80_100_DoubleMuon_2012C
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleMuon2012D.txt --batch=30 --jobname=jpsiTest658_Z_80_100_DoubleMuon_2012D
+
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleElectron2012A.txt --batch=30 --jobname=jpsiTest659_Z_80_100_DoubleElectron_2012A
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleElectron2012B.txt --batch=30 --jobname=jpsiTest659_Z_80_100_DoubleElectron_2012B
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleElectron2012C.txt --batch=30 --jobname=jpsiTest659_Z_80_100_DoubleElectron_2012C
+condor_filelist.perl ZFinder/Event/zfinder_cfg.py Metadata/filelists/ZJpsiSkimDoubleElectron2012D.txt --batch=30 --jobname=jpsiTest659_Z_80_100_DoubleElectron_2012D
